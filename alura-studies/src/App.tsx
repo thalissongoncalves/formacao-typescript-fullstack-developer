6975fb49274251d00e4fc08a8a08f388d410627e
thalissongoncalves <thalissongsilva70@gmail.com>
2024-12-04 17:42:07 -0300

    Feito aula da seção 02

--- alura-studies/src/App.tsx
+++ alura-studies/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import Button from './components/Button';
+import Formulario from './components/Formulario';
+import Lista from './components/Lista';
 
 function App() {
   return (
     <div className="App">
-      <Button />
+      <Formulario />
+      <Lista />
     </div>
   );
 }
