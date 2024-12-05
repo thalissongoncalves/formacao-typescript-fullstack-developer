312fd9c92081fe906da772b0d2e9696e5ad92030
thalissongoncalves <thalissongsilva70@gmail.com>
2024-12-05 17:54:19 -0300

    Feito aulas da seção 03. usando CSS

--- alura-studies/src/components/Lista/index.tsx
+++ alura-studies/src/components/Lista/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import style from './Lista.module.scss';
 
 function Lista() {
     const tarefas = [
@@ -17,11 +18,11 @@ function Lista() {
     ];
 
     return (
-        <aside>
+        <aside className={style.listaTarefas}>
             <h2> Estudos do dia </h2>
             <ul>
                 {tarefas.map((item, index) => (
-                    <li key={index}>
+                    <li key={index} className={style.item}>
                         <h3>
                             {item.tarefa}
                         </h3>
