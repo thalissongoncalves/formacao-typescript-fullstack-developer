b99dd1ff840f33c9bda0ec2f696fb7d64a861357
thalissongoncalves <thalissongsilva70@gmail.com>
2024-12-17 17:53:38 -0300

    .

--- alura-studies/src/components/Formulario/index.tsx
+++ alura-studies/src/components/Formulario/index.tsx
@@ -1,11 +1,39 @@
 import React from "react";
 import Button from "../Button";
 import style from './Formulario.module.scss';
+import { ITarefa } from "../../types/tarefa";
+import { v4 as uuidv4 } from 'uuid';
 
-class Formulario extends React.Component{
+class Formulario extends React.Component<{
+    setTarefas: React.Dispatch<React.SetStateAction<ITarefa[]>>
+}>{
+    state = {
+        tarefa: "",
+        tempo: "00:00"
+    }
+
+    adicionarTarefa(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+        this.props.setTarefas((tarefasAntigas) => 
+            [
+                ...tarefasAntigas,
+                {
+                    ...this.state,
+                    selecionado: false,
+                    completado: false,
+                    id: uuidv4()
+                }
+            ]
+        );
+        this.setState({
+            tarefa: "",
+            tempo: "00:00"
+        })
+    }
+
     render() {
         return (
-            <form className={style.novaTarefa}>
+            <form className={style.novaTarefa} onSubmit={this.adicionarTarefa.bind(this)}>
                 <div className={style.inputContainer}>
                     <label htmlFor="tarefa">
                         Adicione um novo estudo
@@ -14,6 +42,8 @@ class Formulario extends React.Component{
                         type="text"
                         name="tarefa"
                         id="tarefa"
+                        value={this.state.tarefa}
+                        onChange={(e) => this.setState({ ...this.state, tarefa: e.target.value })}
                         placeholder="O que você quer estudar"
                         required
                     />
@@ -26,6 +56,8 @@ class Formulario extends React.Component{
                         type="time"
                         step="1"
                         name="tempo"
+                        value={this.state.tempo}
+                        onChange={(e) => this.setState({ ...this.state, tempo: e.target.value})}
                         id="tempo"
                         min="00:00:00"
                         max="01:30:00"
@@ -33,6 +65,7 @@ class Formulario extends React.Component{
                     />
                 </div>
                 <Button
+                    type="submit"
                     texto="Adicionar"
                 />
             </form>
