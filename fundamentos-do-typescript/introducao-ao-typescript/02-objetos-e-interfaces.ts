1a7513a728112bad2ec2e836f17f4d05d0cf17cc
Thalisson Gonçalves <thalissongsilva70@gmail.com>
2024-12-28 20:12:02 -0300

    Feito aula 02

---
+++ fundamentos-do-typescript/introducao-ao-typescript/02-objetos-e-interfaces.ts
@@ -0,0 +1,31 @@
+export {};
+
+interface Pessoa {
+    name: string,
+    idade: number,
+    profissao?: string
+};
+
+const pessoa: Pessoa = {
+    name: 'Thalisson',
+    idade: 22
+};
+
+const pessoa2: Pessoa = {
+    name: 'Shaiane',
+    idade: 21,
+    profissao: 'Costureira'
+};
+
+const arrayPessoas: Array<Pessoa> = [
+    pessoa,
+    pessoa2
+];
+
+const arrayNumber: number[] = [
+    1, 2, 3
+];
+
+const arrayString: Array<string> = [
+    '1', '2', '3'
+];
