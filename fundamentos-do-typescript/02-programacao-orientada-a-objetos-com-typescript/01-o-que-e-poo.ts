e56a0c2b53c9eeb55e5912d7b4ae1b8185c4c955
Thalisson Gonçalves <thalissongsilva70@gmail.com>
2025-02-12 01:13:04 -0300

    Feito a aula 01 do curso Programaçãomação Orientada a Objetos

---
+++ fundamentos-do-typescript/02-programacao-orientada-a-objetos-com-typescript/01-o-que-e-poo.ts
@@ -0,0 +1,19 @@
+class User {
+  name: string = 'Nath'
+  age: number = 32
+
+  constructor(name: string, age: number) {
+    this.name = name
+    this.age = age
+  }
+
+  showName = () => {
+    console.log(this.name)
+  }
+}
+
+const user = new User('Thalisson', 22)
+user.showName()
+
+const otherUser = new User('Shaiane', 21)
+otherUser.showName()
