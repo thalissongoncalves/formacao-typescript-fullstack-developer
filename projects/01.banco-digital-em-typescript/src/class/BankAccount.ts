e2e0b2a37b828d33bac9ededb5bbbf25a23859b8
Thalisson Gonçalves <thalissongsilva70@gmail.com>
2025-02-18 17:24:00 -0300

    Feito o requisito 2 (Deve conter a propriedade name: privado e imutável, accountNumber: privado e imutável, balance: privado e status: privado, iniciado como true)

--- projects/01.banco-digital-em-typescript/src/class/BankAccount.ts
+++ projects/01.banco-digital-em-typescript/src/class/BankAccount.ts
@@ -1,3 +1,11 @@
 export abstract class BankAccount {
-  
+  private readonly name: string;
+  private readonly accountNumber: number;
+  private balance: number = 0;
+  private status: boolean = true;
+
+  constructor(name: string, accountNumber: number) {
+    this.name = name
+    this.accountNumber = accountNumber
+  }
 }
