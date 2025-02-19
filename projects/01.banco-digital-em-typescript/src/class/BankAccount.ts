2c7f0b073732903fa4d37cce4c84f87053919cb4
Thalisson Gonçalves <thalissongsilva70@gmail.com>
2025-02-19 16:00:59 -0300

    Feito o requisito 3 (Deve o conter os métodos: deposit() que adiciona saldo a conta | withdraw() que retira saldo da conta se houver fundos suficientes | transfer() que transfere dinheiro para outra conta | getBalance() que retorna o saldo | validateStatus() que verifica se a conta está ativa)

--- projects/01.banco-digital-em-typescript/src/class/BankAccount.ts
+++ projects/01.banco-digital-em-typescript/src/class/BankAccount.ts
@@ -1,4 +1,4 @@
-export abstract class BankAccount {
+export class BankAccount {
   private readonly name: string;
   private readonly accountNumber: number;
   private balance: number = 0;
@@ -7,5 +7,40 @@ export abstract class BankAccount {
   constructor(name: string, accountNumber: number) {
     this.name = name
     this.accountNumber = accountNumber
+  }
+
+  // Adiciona saldo à conta
+  deposit = (value: number): string => {
+    this.balance += value
+    return `Depósito feito com sucesso!`
+  }
+
+  // Retira saldo se houver fundos suficientes
+  withdraw = (value: number): string => {
+    if (value <= this.balance) {
+      this.balance -= value
+    } else {
+      return `Seu saldo é insuficiente: R$${this.balance}.`
+    }
+    return `Saque feito com sucesso!`
+  }
+
+  // Transfere dinheiro para outra conta
+  transfer = (value: number, targetAccount: BankAccount) => {
+    targetAccount.balance += value
+  }
+
+  // Retorna o saldo
+  getBalance = () => {
+    return this.balance
+  }
+
+  // Verifica se a conta está ativa
+  validateStatus = () => {
+    if (!this.status) {
+      throw new Error("Conta inválida");
+    }
+    
+    return true
   }
 }
