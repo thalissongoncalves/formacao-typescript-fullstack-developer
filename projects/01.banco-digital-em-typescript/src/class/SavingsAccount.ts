97b339c924aa6e4fa6cf2e1aa63600060a3a3578
Thalisson Gonçalves <thalissongsilva70@gmail.com>
2025-02-19 17:00:38 -0300

    2. Criar três tipos de contas bancárias: SavingsAccount (Conta Poupança) | Herda de BankAccount.

---
+++ projects/01.banco-digital-em-typescript/src/class/SavingsAccount.ts
@@ -0,0 +1,9 @@
+import { BankAccount } from "./BankAccount";
+
+export class SavingsAccount extends BankAccount {
+  constructor(name: string, accountNumber: number) {
+    super(name, accountNumber)
+  }
+
+  
+}
