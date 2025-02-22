31b3b066665b36900a36643a9506d230676d6679
Thalisson Gonçalves <thalissongsilva70@gmail.com>
2025-02-22 20:53:44 -0300

    3. Criar um arquivo app.ts para testar o sistema | Criar instâncias das três contas

---
+++ projects/01.banco-digital-em-typescript/app.ts
@@ -0,0 +1,7 @@
+import { BusinessAccount } from "./src/class/BusinessAccount";
+import { PremiumAccount } from "./src/class/PremiumAccount";
+import { SavingsAccount } from "./src/class/SavingsAccount";
+
+const businessAccount = new BusinessAccount("Thalisson", 100)
+const premiumAccount = new PremiumAccount("Shaiane", 200)
+const savingsAccount = new SavingsAccount("Claudilene", 300)
